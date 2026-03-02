af84b347701843b61cab4e9e4814ec434468a647
Ayesha-te <ayeshajahangirrr@gmail.com>
2026-03-02 20:13:06 +0500

    wqeqw

--- core/settings.py
+++ core/settings.py
@@ -158,6 +158,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/media'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
